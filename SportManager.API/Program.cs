ed08a9d710902a122b2c2c1e612d431a23b757f7
doannc@ichiba.vn <doannc@ichiba.vn>
2025-06-20 09:42:50 +0700

    update cors

--- SportManager.API/Program.cs
+++ SportManager.API/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowLocalhost",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+            policy.WithOrigins("http://localhost:3000", "http://localhost:5173", "https://sportmanage-api.onrender.com")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
